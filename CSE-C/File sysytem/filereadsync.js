4c2efe5dfa9222884595fe3a71d20c4c6eaab082
shubhchaudhary07 <shubhrawat718@gmail.com>
2025-02-11 10:11:14 +0530

    update data 11/02/2025

--- CSE-C/File sysytem/filereadsync.js
+++ CSE-C/File sysytem/filereadsync.js
@@ -1,3 +1,17 @@
 const fs = require("fs");
-const data = fs.readFileSync("./data.txt");
+//fs.writeFileSync("./data1.txt","Hello ABES","utf-8");
+//fs.writeFileSync("./data1.txt","Hello ABESEC","utf-8");
+//fs.appendFileSync("./data1.txt"," students","utf-8");
+
+//fs.renameSync("./data2.txt","./data3.txt");
+
+//fs.unlinkSync("./data3.txt");
+
+const data = fs.readFileSync("./data.txt","utf-8");
+//console.log(data.toString());
 console.log(data)
+if(data.match("H")){
+    console.log("File contains 'H'");
+    const newdata = data.replace("H", "ABES");
+    fs.writeFileSync("./data.txt", newdata,"utf-8");
+}
